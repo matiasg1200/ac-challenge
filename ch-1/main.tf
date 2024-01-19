a9176ca8be752d3b061885582bfce0c217df8165
Matias <matiasgabriel1200@gmail.com>
2024-01-19 19:32:44 -0300

    first commit

---
+++ ch-1/main.tf
@@ -0,0 +1,70 @@
+# Create Windows compute instance
+resource "google_compute_instance" "instance-1" {
+  name         = "instance-1"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-1"
+    initialize_params {
+      image = "projects/windows-cloud/global/images/windows-server-2022-dc-v20240111"
+      size  = 50
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+  metadata = {
+    enable-windows-ssh         = "TRUE"
+    windows-startup-script-url = "https://storage.googleapis.com/ac-devops-chg-01-2024/startup.ps1"
+  }
+
+  scheduling {
+    automatic_restart  = false
+    preemptible        = true
+    provisioning_model = "SPOT"
+  }
+
+  service_account {
+    email  = "285657077755-compute@developer.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+
+  depends_on = [
+    google_storage_bucket.bucket,
+    google_storage_bucket_object.object
+  ]
+
+}
+
+# Create public access Storage Bucket for startup script 
+resource "google_storage_bucket_access_control" "bucket_rule" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "ac-devops-chg-01-2024"
+  location = "US"
+}
+
+# Upload script to Storage Bucket
+resource "google_storage_object_access_control" "object_rule" {
+  object = google_storage_bucket_object.object.output_name
+  bucket = google_storage_bucket.bucket.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+resource "google_storage_bucket_object" "object" {
+  name   = "startup.ps1"
+  source = "./scripts/startup.ps1"
+  bucket = google_storage_bucket.bucket.name
+}
+
---
+++ ch-1/scripts/startup.ps1
@@ -0,0 +1,13 @@
+# Install Choco
+Set-ExecutionPolicy AllSigned
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
+
+# Install Packages
+choco install git.install -y
+choco install nodejs.install -y
+choco install office365business -y
+choco install openjdk -y
+
+# Enable Windows SSH
+googet -noconfirm=true update google-compute-engine-windows 
+googet -noconfirm=true install google-compute-engine-ssh
