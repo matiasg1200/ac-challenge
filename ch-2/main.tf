f1344404d409c0425dbd7b409627643426ea1244
Matias <matiasgabriel1200@gmail.com>
2024-01-21 13:15:08 -0300

    Adding initial resoruce files and configs for ch2

---
+++ ch-2/main.tf
@@ -0,0 +1,24 @@
+resource "google_sourcerepo_repository" "source-repo" {
+  name = "source-devops-repo"
+}
+
+resource "google_artifact_registry_repository" "artifcat-repo" {
+  location      = "us-central1"
+  repository_id = "artifact-devops-repo"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
+}
+
+resource "google_cloudbuild_trigger" "trigger" {
+  location = "us-central1"
+  filename = "cloudbuild.yaml"
+
+  trigger_template {
+    branch_name = "main"
+    repo_name   = google_sourcerepo_repository.source-repo.name
+  }
+
+}
