a7b52e7c655b99352321425070d2b83311313453
Matias <matiasgabriel1200@gmail.com>
2024-01-25 15:41:37 -0300

    updating variables

---
+++ ch-2/terraform.tfvars
@@ -0,0 +1,9 @@
+iam_roles = {
+  "act_as"      = "roles/iam.serviceAccountUser"
+  "cloud_repo"  = "roles/"
+  "cloud_build" = "roles/cloudbuild.builds.builder"
+  "cloud_run"   = "roles/run.admin"
+  "write_logs"  = "roles/logging.logWriter"
+}
+
+
--- ch-2/variables.tf
+++ ch-2/variables.tf
@@ -2,3 +2,8 @@ variable "project_id" {
   description = "GCP project Id to deploy resoruces"
   type = string
 }
+
+variable "iam_roles" {
+  description = "A list of the required roles for the Service account to deploy on Cloud Run"
+  type = map
+}
